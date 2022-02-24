1d5f66a4d2e6cab6c1a725c1133430b96bc3e352
Anton Demkin <antondemkin@yandex.ru>
2022-02-24 22:38:10 +0200

    Actualize webinar

--- webinar.py
+++ webinar.py
@@ -1,32 +1,30 @@
 from datetime import datetime
 from pathlib import Path
-import sys
+from time import sleep
 
-from gspread import service_account
 from gspread import Spreadsheet
 from gspread import Worksheet
 from gspread.exceptions import WorksheetNotFound
-from loguru import logger
 
 from images import Certificate
+from loguru import logger
 from participants import Participant
+from send_email import AbstractMail
 from send_email import GMail
 from send_email import MailStub
+from sheets import get_participants_from_sheet
+from sheets import get_webinar_date_and_title
+from sheets import open_spreadsheet
 from word_morph import Morph
 
-URL_BASE = "https://docs.google.com/spreadsheets/d/"
-URL = URL_BASE + "1kY2oVoqIK_5pc_wn6ZjZtojoQa0xnWNOd-SCHyHtGHU"  # copy
-CERTIFICATES = "сертификаты"
-PARTICIPANTS = "участники"
-EMAIL_MESSAGE_TEMPLATE = """Hello, {name}!
 
-Thank you for participating in my webinar!
-
-Your certificate is in attachment.
-{custom_text}
-
-Regards, Python
-"""
+URL = (
+    "https://docs.google.com/spreadsheets/d/"
+    "18k_3stoWisJEzebVbnqcviJgLZsYmy_WFpNSgWc-QSc"
+)
+URL = "https://docs.google.com/spreadsheets/d/1mj6-i4uC5HM_v-WLB3jgo22SvruULnawCNxlkGgWDIU/edit?usp=sharing"
+CERTIFICATES = "mailing"
+PARTICIPANTS = "Form Responses 1"
 
 
 class Webinar:
@@ -39,7 +37,7 @@ class Webinar:
             year: int,
             certs_dir: Path,
             cert_template: Path,
-            email: GMail,
+            email: AbstractMail,
     ) -> None:
         self.document: Spreadsheet = document
         self.participants: list[Participant] = participants
@@ -53,24 +51,25 @@ class Webinar:
 
     @classmethod
     def from_url(cls, url: str = URL) -> 'Webinar':
-        document = create_spreadsheet(url)
+        document = open_spreadsheet(url)
         logger.debug("creating webinar")
-        participants_sheet = document.worksheet('участники')
+        # get participants
         participants = get_participants_from_sheet(
-            participants_sheet,
+            document.worksheet(PARTICIPANTS),
             first_row=1,
         )
-        title = get_webinar_topic_from_sheet(participants_sheet)
-        date_str = get_webinar_date_from_sheet(participants_sheet)
+        # get title and date
+        date_str, title = get_webinar_date_and_title(document)
         year = datetime.now().year
-        cert_template = Path("template_without_date.jpeg")
-        if not cert_template.exists():
-            logger.error(f"{cert_template} not found")
-            sys.exit(1)
+        # get certificates data
+        # cert_template = Path("template_speech.jpeg")
+        cert_template = Path("template_grammar.jpeg")
         certs_parent_dir = Path('.') / 'certificates'
         certs_parent_dir.mkdir(exist_ok=True)
         certs_dir = certs_parent_dir / f"{date_str}-{year}"
         certs_dir.mkdir(exist_ok=True)
+        # create mailer
+        # email = MailStub()
         email = GMail.from_environ()
         return cls(
             document=document,
@@ -91,6 +90,7 @@ class Webinar:
     def cert_sheet(self) -> Worksheet:
         headers = ['fio', 'given_fio', 'name', 'email', 'custom_text']
         # TODO: add instagram after email
+
         if self._cert_sheet is None:
             try:
                 self._cert_sheet = self.document.worksheet(CERTIFICATES)
@@ -110,10 +110,10 @@ class Webinar:
             logger.debug("already filled")
             return True
         elif len(cert_sheet_rows) > len(self.participants):
-            logger.warning("already filled but rows > participants, need checking")
+            logger.warning("already filled but rows > participants")
             return True
         elif 0 < len(cert_sheet_rows) < len(self.participants):
-            logger.error("looks like table is filled up partially, unable to process")
+            logger.error("looks like table is filled up partially")
         return False
 
     def certificates_sheet_fill(self) -> None:
@@ -132,12 +132,13 @@ class Webinar:
                 logger.exception(f"Unable to morph {participant.fio}", err)
                 fio_given = ''
                 name = ''
+            message = f'Здравствуйте, {name}! Благодарю вас за участие.'
             row = [
                 participant.fio,
                 fio_given,
                 name,
                 participant.email,
-                '',
+                message,
             ]
             self.cert_sheet.append_row(row)
             logger.info(f"{participant.fio} done")
@@ -149,22 +150,22 @@ class Webinar:
         for row in self.cert_sheet.get_all_values():
             given_fio = row[1]
             logger.debug(f"{given_fio} taken")
-            cert = Certificate.create(
+            certificate = Certificate.create(
                 template=self.cert_template,
                 certs_dir=self.certs_dir,
                 name=given_fio,
                 date=self.date_str,
                 year=self.year,
             )
-            cert.create_file()
-            logger.debug(f"{given_fio} cert path: {str(cert.path)}")
+            certificate.create_file()
+            logger.debug(f"{given_fio} cert path: {str(certificate.path)}")
         logger.info("generating certs done")
 
     def send_emails_with_certificates(self) -> None:
         # headers = ['name', 'given_name', 'just_name', 'email', 'custom_text']
         logger.info("sending emails")
         for row in self.cert_sheet.get_all_values():
-            fio, given_fio, name, email, message = row
+            fio, given_fio, _, email, message = row
             logger.debug(f"{fio} taken")
             cert = Certificate.create(
                 template=self.cert_template,
@@ -177,7 +178,7 @@ class Webinar:
                 logger.debug(f"{fio} generating cert")
                 cert.create_file()
                 logger.debug(f"{fio} generating cert done")
-            message = message.format(name=name)
+            # message = message.format(name=name)
             logger.info(f"{fio} sending email to {email}")
             # Hack to send files with latin name
             new_file = "/tmp/certificate.jpeg"
@@ -193,14 +194,18 @@ class Webinar:
             )
             logger.info(f"{fio} done")
             # TODO: mark email as sent in google sheet
+            sleep(3)
         logger.info("sending emails done")
 
 
 if __name__ == '__main__':
     print(URL)
     # tonyflexmusic@gmail.com jnrbviavjvpxtogz
-    # GMAILACCOUNT="milabaltyca@gmail.com" GMAILAPPLICATIONPASSWORD="ybullixoirpowibr"
+    # GMAILACCOUNT="milabaltyca@gmail.com"
+    # GMAILAPPLICATIONPASSWORD="ybullixoirpowibr"
     webinar = Webinar.from_url(URL)
     # webinar.certificates_sheet_fill()
+    # make sure that names transformed correctly
     # webinar.certificates_generate()
+    # make sure that certificates are correct
     # webinar.send_emails_with_certificates()
