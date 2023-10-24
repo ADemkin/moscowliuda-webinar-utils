180961673ff53cd1d7508748c9ed2d97696c764a
Anton Demkin <avdemkin@avito.ru>
2023-10-24 22:13:38 +0400

    Show exception in web ui

--- lib/webui/app.py
+++ lib/webui/app.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
 from aiohttp.web import Application
-from aiohttp.web import RouteTableDef
 from aiohttp.web import run_app
+from aiohttp.web import middleware
+from aiohttp.web import Request
+from aiohttp.web import Response
+from aiohttp.web import HTTPException
+from aiohttp.web import Callable
 import aiohttp_jinja2
 import jinja2
 
@@ -10,7 +14,29 @@ from lib.storage import WebinarStorage
 from lib.webui.routes import setup_routes
 
 CWD = Path(".")
-routes = RouteTableDef()
+
+
+@middleware
+async def handle_exception_middleware(
+        request: Request,
+        handler: Callable,
+) -> Response:
+    try:
+        return await handler(request)
+    except HTTPException as err:
+        raise err
+    except Exception as err:
+        context = {
+            'error': {
+                'title': err.__class__.__name__,
+                'message': str(err),
+            },
+        }
+        return aiohttp_jinja2.render_template(
+            template_name='error.html',
+            request=request,
+            context=context,
+        )
 
 
 def create_app() -> Application:
@@ -23,6 +49,9 @@ def create_app() -> Application:
         app=app,
         loader=jinja2.FileSystemLoader(templates_dir),
     )
+    # setup middleware
+    app.middlewares.append(handle_exception_middleware)
+    # setup storage
     storage = WebinarStorage()
     app['storage'] = storage
     return app
