ec72e2f7465ed5697ad6d29aad50c4f8853734a5
Anton Demkin <avdemkin@avito.ru>
2024-01-21 13:03:50 +0400

    Add webinar CLI tool

---
+++ bin/webinar.py
@@ -0,0 +1,49 @@
+import click
+from dotenv import load_dotenv
+
+from lib.webinar import Webinar
+
+
+@click.group()
+def cli() -> None:
+    load_dotenv()
+
+
+@cli.command()
+@click.argument("url")
+def contacts(url: str) -> None:
+    click.echo(f"Importing contacts from {url}")
+    click.confirm("Continue?", default=True, abort=True)
+    path = Webinar.from_url(url).import_contacts()
+    click.echo(f"Contacts saved to {click.format_filename(path)}")
+    click.echo("Import this file using icloud.com")
+    if click.confirm("Open directory with contacts?", default=True):
+        click.launch(str(path.parent))
+
+
+@cli.command()
+@click.argument("url")
+def fill(url: str) -> None:
+    click.echo(f"Fill mailing sheet from {url}")
+    click.confirm("Continue?", default=True, abort=True)
+    Webinar.from_url(url).certificates_sheet_fill()
+    click.echo("Mailing sheet filled")
+    if click.confirm("Open mailing sheet?", default=True):
+        click.launch(url)
+
+
+@cli.command()
+@click.argument("url")
+def send(url: str) -> None:
+    click.echo(f"Send emails with certificates from {url}")
+    if click.confirm("Open mailing sheet?", default=True):
+        click.launch(url)
+    if click.confirm("Test emails?", default=True):
+        Webinar.from_url(url, test=True).send_emails_with_certificates()
+    if click.confirm(click.style("Send emails?", fg="red"), abort=True):
+        Webinar.from_url(url).send_emails_with_certificates()
+        click.echo("Emails sent")
+
+
+if __name__ == "__main__":
+    cli()
