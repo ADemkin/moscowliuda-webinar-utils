85dedb456475cf72c41ee5254837602a561a6857
Anton Demkin <avdemkin@avito.ru>
2024-01-21 11:48:54 +0400

    Fix lint in environment field factory

--- lib/environment.py
+++ lib/environment.py
@@ -1,10 +1,8 @@
 from dataclasses import field
 from functools import partial
 from os import environ
+from typing import Any
 from typing import Callable
-from typing import TypeVar
-
-T = TypeVar("T")
 
 
 class EnvironmentVariableNotSetError(Exception):
@@ -12,22 +10,20 @@ class EnvironmentVariableNotSetError(Exception):
 
 
 def get_env_variable(
-    factory: Callable[[str], T],
+    cast: Callable,
     var_name: str,
-    default: str = None,
-) -> T:
+    default: Any | None = None,
+) -> str:
     if (value := environ.get(var_name, default)) is not None:
-        return factory(value)
+        return cast(value)
     raise EnvironmentVariableNotSetError(f"Environment variable {var_name!r} is not set")
 
 
-get_env_str_var = partial(get_env_variable, factory=str)
-
-
-def env_str_field(var_name: str, default: str = None) -> str:
+def env_str_field(var_name: str, default: str | None = None) -> str:
     return field(
         default_factory=partial(
-            get_env_str_var,
+            get_env_variable,
+            cast=str,
             var_name=var_name,
             default=default,
         ),
