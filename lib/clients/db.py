e4542e66128fe85fa394fc496358fb0e77cf7397
Anton Demkin <avdemkin@avito.ru>
2024-01-22 19:43:49 +0400

    Update db and add tests

--- lib/clients/db.py
+++ lib/clients/db.py
@@ -1,38 +1,41 @@
 from contextlib import contextmanager
 from dataclasses import dataclass
 from dataclasses import field
+from functools import partial
 from pathlib import Path
 from sqlite3 import Connection
 from sqlite3 import connect
 from typing import Generator
+from typing import Sequence
 
 from lib.environment import env_str_field
 from lib.paths import DB_PATH
 
 
-@dataclass
+def discover_migrations(migrations_dir: Path) -> Sequence[str]:
+    migration_paths = sorted(migrations_dir.glob("*.sql"))
+    return [path.read_text() for path in migration_paths]
+
+
+@dataclass(frozen=True, slots=True)
 class DB:
-    path: str = env_str_field("DBPATH", "db.sqlite3")
-    migrations: Path = field(default=DB_PATH / "migrations")
-    timeout: int = 10
-
-    @classmethod
-    def create_in_memory(cls) -> "DB":
-        return cls(path=":memory:")
-
-    def _migrate(self) -> None:
-        migration_paths = sorted(self.migrations.glob("*.sql"))
-        migration_queries = [path.read_text() for path in migration_paths]
-        with self.connection() as connection:
-            for migration_query in migration_queries:
-                connection.executescript(migration_query)
+    path: str | Path = env_str_field("DBPATH", "db.sqlite3")  # or ":memory:"
+    migrations: Sequence[str] = field(
+        default_factory=partial(discover_migrations, DB_PATH / "migrations"),
+    )
+    timeout: int = 5
 
     def __post_init__(self) -> None:
-        self._migrate()
+        with self.connection() as connection:
+            for migration in self.migrations:
+                connection.executescript(migration)
+
+    def get_connection(self) -> Connection:
+        return connect(self.path, timeout=self.timeout)
 
     @contextmanager
     def connection(self) -> Generator[Connection, None, None]:
-        connection = connect(self.path, timeout=self.timeout)
+        connection = self.get_connection()
         try:
             yield connection
         except:  # noqa
---
+++ tests/clients/test_db.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+from typing import Generator
+
+import pytest
+
+from lib.clients.db import DB
+
+
+@pytest.fixture(scope="function")
+def db(tmp_path: Path) -> Generator[DB, None, None]:
+    query = """
+        DROP TABLE IF EXISTS test;
+        CREATE TABLE IF NOT EXISTS test (
+            id INTEGER PRIMARY KEY,
+            name TEXT
+        );
+    """
+    yield DB(
+        path=tmp_path / "test.sqlite3",
+        migrations=[query],
+    )
+
+
+def test_db_migrations_applied_on_init(tmp_path: Path) -> None:
+    migrations = [
+        "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)",
+        "CREATE TABLE IF NOT EXISTS test2 (id INTEGER PRIMARY KEY, name TEXT)",
+    ]
+    db = DB(
+        path=tmp_path / "test.sqlite3",
+        migrations=migrations,
+    )
+    with db.connection() as connection:
+        connection.execute("INSERT INTO test (name) VALUES ('a')")
+        connection.execute("INSERT INTO test2 (name) VALUES ('a')")
+
+
+def test_db_commit_successfull_transaction(db: DB) -> None:
+    with db.connection() as connection:
+        query = "INSERT INTO test (name) VALUES (:name)"
+        params = {"name": "test"}
+        connection.execute(query, params)
+    with db.connection() as connection:
+        row = connection.execute("SELECT COUNT(*) FROM test").fetchone()
+    assert row[0] == 1
+
+
+def test_db_drop_failed_transaction(db: DB) -> None:
+    def _raise_inside_transaction() -> None:
+        with db.connection() as connection:
+            query = "INSERT INTO test (name) VALUES (:name)"
+            params = {"name": "test"}
+            raise Exception("Test exception")
+            connection.execute(query, params)
+
+    with pytest.raises(Exception):
+        _raise_inside_transaction()
+
+    with db.connection() as connection:
+        row = connection.execute("SELECT COUNT(*) FROM test").fetchone()
+    assert row[0] == 0
